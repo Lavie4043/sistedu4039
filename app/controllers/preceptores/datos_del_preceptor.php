40b5b7ee13c6acf3a4c10b9e3b5f952d22ba1bc5
pjgb-git <paulajgarcia68@gmail.com>
2024-11-21 21:22:51 -0300

    preceptores asignaciones

--- app/controllers/preceptores/datos_del_preceptor.php
+++ app/controllers/preceptores/datos_del_preceptor.php
@@ -3,7 +3,7 @@ $sql_preceptores = "SELECT * FROM usuarios as usu
 INNER JOIN roles as rol ON rol.id_rol = usu.rol_id 
 INNER JOIN personas as per ON per.usuario_id = usu.id_usuario 
 INNER JOIN preceptores as pre On pre.persona_id = per.id_persona WHERE pre.estado = '1' AND pre.id_preceptore = '$id_preceptore'";
-//AND doc.id_docente = '$id_docente'
+
 $query_preceptores = $pdo->prepare($sql_preceptores);
 $query_preceptores->execute();
 $preceptores = $query_preceptores->fetchAll(PDO::FETCH_ASSOC);
--- app/controllers/preceptores/listado_de_asignaciones.php
+++ app/controllers/preceptores/listado_de_asignaciones.php
@@ -1,14 +1,21 @@
 <?php
 $sql_asignaciones = "SELECT * FROM asignaciones as asi
-INNER JOIN docentes as doc ON doc.id_docente = asi.docente_id 
-INNER JOIN personas as per ON per.id_persona = doc.persona_id 
+INNER JOIN preceptores as pre ON pre.id_preceptore = asi.preceptore_id 
+INNER JOIN personas as per ON per.id_persona = pre.persona_id 
 INNER JOIN usuarios as usu ON usu.id_usuario = per.usuario_id 
 INNER JOIN niveles as niv ON niv.id_nivel = asi.nivel_id 
 INNER JOIN grados as gra On gra.id_grado = asi.grado_id 
-INNER JOIN materias as mat On mat.id_materia = asi.materia_id 
+
+
 
 WHERE asi.estado = '1' ";
 
 $query_asignaciones = $pdo->prepare($sql_asignaciones);
 $query_asignaciones->execute();
 $asignaciones = $query_asignaciones->fetchAll(PDO::FETCH_ASSOC);
+
+ 
+
+
+
+
