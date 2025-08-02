131f550b78759541c23f6b2a3f8792cc1019de4e
Lavie Informatica <lavieinf@uutlook.com>
2025-08-02 10:03:52 -0300

    intentando integrar

--- app/controllers/calificaciones/listado_de_calificaciones.php
+++ app/controllers/calificaciones/listado_de_calificaciones.php
@@ -1,5 +1,4 @@
 <?php
-<<<<<<< HEAD
 $sql_calificaciones = "SELECT 
     cal.*, 
     mat.nombre_materia, 
@@ -17,13 +16,4 @@ $calificaciones = $query_calificaciones->fetchAll(PDO::FETCH_ASSOC);
    
 
 
-=======
 
-$sql_calificaciones = "SELECT * FROM calificaciones as cal
-INNER JOIN materias as mat ON mat.id_materia = cal.materia_id
-WHERE cal.estado = '1' ";
-
-$query_calificaciones = $pdo->prepare($sql_calificaciones);
-$query_calificaciones->execute();
-$calificaciones = $query_calificaciones->fetchAll(fetch_style: PDO::FETCH_ASSOC);
->>>>>>> b293d0af06a5c19534cfa1d275279fd522e5cdb8
